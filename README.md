99fc2024d1736a1538e69eeef487ea51c69c40af
Patrick Ceriale <patceriale@gmail.com>
2021-01-13 17:27:32 -0800

    created index.js and added some npm packages

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Repertoire-Back-End
-Back End for repertoire recipes
+# **Repetoire Recipes**
+
+![License badge](https://img.shields.io/badge/license-None-success)
+
+## **Description**
+
+> This is the back end for Repertoire Recipes (https://github.com/PatCeriale/repertoire-recipes).
+
+## **Table of Contents**
+
+1. [Installation](#installation)
+1. [Usage](#usage)
+1. [License](#license)
+1. [Questions](#questions)
+1. [Preview](#preview)
+
+## **Installation**
+
+> Enter npm install in command line
+
+## **Usage**
+
+> Enter your favorite recipes uding the front end application, update any changes and view them while cooking to have a meal momma would be proud of
+
+## **License**
+
+> None
+
+## **Questions**
+
+Find me on GitHub at https://github.com/PatCeriale.
+Contact me via email at Patceriale.
+
+## **Preview**
+
+coming soon
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const bcrypt = require("bcrypt");
+
+const allRoutes = require("./routes");
+
+const db = require("./models");
+// const { User } = require("./models");
+
+const PORT = process.env.PORT || 5000;
+
+// const User = require("./Models/userModel.js");
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(
+  cors({
+    origin: [
+      "https://truckntastyfood.herokuapp.com",
+      "http://localhost:3001",
+      "http://localhost:3000",
+    ],
+  })
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+app.use("/", allRoutes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/foodtruck", {
+  useNewUrlParser: true,
+});
+mongoose.set("useFindAndModify", false);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
